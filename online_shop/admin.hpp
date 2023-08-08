229c7ab089aa2e18b723aa0f714bb8cccb13a39b
bara <92505565+wakeawakee@users.noreply.github.com>
2023-08-08 17:13:25 +0545

    Create admin.hpp

---
+++ online_shop/admin.hpp
@@ -0,0 +1,21 @@
+
+#pragma once
+#include <iostream>
+#include "User.hpp"
+#include "Product.hpp"
+#include <vector>
+
+class Admin
+{
+private:
+    std::vector<User> users;
+    std::vector<Product> products;
+
+public:
+    Admin();
+
+    void addUser(const User &user);
+    void addProduct(const Product &product);
+
+    void saveDataToFile(const std::string &filename);
+};
